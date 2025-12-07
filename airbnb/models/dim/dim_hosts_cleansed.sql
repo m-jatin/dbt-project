2a0300c694f9abb0ec38798b08095c7d0dd4db49
m-jatin <m.jatin@hotmail.com>
2025-12-07 09:31:33 +0530

    Adding dimension tables

---
+++ airbnb/models/dim/dim_hosts_cleansed.sql
@@ -0,0 +1,14 @@
+WITH src_hosts AS (
+  SELECT
+    *
+  FROM
+    {{ ref('src_hosts') }}
+)
+SELECT
+    host_id,
+    nvl(host_name,'Anonymous') AS host_name,
+    IS_SUPERHOST,
+    CREATED_AT,
+    UPDATED_AT
+FROM
+  src_hosts
